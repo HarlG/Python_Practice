580fd983f43631cf57e592dbb9561d04c4c0a429
Harlan Gillespie <harlan.gillespie1@gmail.com>
2022-03-15 21:52:11 +0000

    Dice game played 500 times. Outputs distribution of results as well as paths of each game

--- random_walk_game/random_walk_game.py
+++ random_walk_game/random_walk_game.py
@@ -1,29 +1,48 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+#Create seed
 np.random.seed(123)
 
+#Initialise of all walks
+all_walks = []
+for i in range(500):
+# Initialisation of single walks
+    random_walk = [0]
 
-# Initialization
-random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
 
-for x in range(100) :
-    step = random_walk[-1]
-    dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step += 1
+        else:
+            step = step + np.random.randint(1,7)
 
-    if dice <= 2:
-        step = max(0, step - 1)
-    elif dice <= 5:
-        step = step + 1
-    else:
-        step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
 
-    random_walk.append(step)
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
 
-# Import matplotlib.pyplot as plt
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1,:]
 
-import matplotlib.pyplot as plt
+
+
 # Plot random_walk
-plt.plot(random_walk)
+plt.subplot(2,1,1)
+plt.plot(np_aw_t)
+plt.xlabel('Dice rolls')
+plt.ylabel('N of Steps')
+
+# Plot histogram of ends, display plot
+plt.subplot(2,1,2)
+plt.hist(ends)
+plt.xlabel('Total steps')
 
 # Show the plot
+plt.tight_layout()
 plt.show()
