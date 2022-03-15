fa21014938753c1f2253439da4d9643779c95ac0
Harlan Gillespie <harlan.gillespie1@gmail.com>
2022-03-15 21:56:13 +0000

    New random 0 rule enacted. More bins in histogram

--- random_walk_game/random_walk_game.py
+++ random_walk_game/random_walk_game.py
@@ -13,13 +13,16 @@ for i in range(500):
     for x in range(100) :
         step = random_walk[-1]
         dice = np.random.randint(1,7)
-
+        # Rules of the game enacted
         if dice <= 2:
             step = max(0, step - 1)
         elif dice <= 5:
             step += 1
         else:
             step = step + np.random.randint(1,7)
+        # 0.1% chance of going to 0
+        if np.random.rand() <= 0.001 :
+            step = 0
 
         random_walk.append(step)
     all_walks.append(random_walk)
@@ -40,8 +43,9 @@ plt.ylabel('N of Steps')
 
 # Plot histogram of ends, display plot
 plt.subplot(2,1,2)
-plt.hist(ends)
+plt.hist(ends, bins= 20)
 plt.xlabel('Total steps')
+plt.ylabel('Frequency')
 
 # Show the plot
 plt.tight_layout()
